9c0e07792b1fce06e0cc809b5ef54354816884a3
WildHashGhost-Cica <wildhashghost@gmail.com>
2022-10-29 20:41:49 +0100

    entry point

--- src/bin/value_of_Instantiate.rs
+++
@@ -1,9 +0,0 @@
-use cosmwasm_std::entry_point;
-use cosmwasm_std::{Response};
-use crate::error::ContractError;
-
-#[entry_point]
-pub fn instantiate() -> Result<Response, ContractError>/* signature return here */ {
-    Ok(Response::default())
-    // actual return here
-}
